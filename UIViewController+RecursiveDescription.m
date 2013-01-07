da20ed14c014ffcffb5cd1688e9e1cfb00d6fcbb
jrturton <richard.turton@moo-14-mac.moo.mubaloo.com>
2013-01-07 09:58:03 +0000

    Initial commit

---
+++ UIViewController+RecursiveDescription.m
@@ -0,0 +1,33 @@
+//
+//  UIViewController+RecursiveDescription.m
+//  HLiPad
+//
+//  Created by Richard Turton on 07/01/2013.
+//
+//
+
+#import "UIViewController+RecursiveDescription.h"
+
+@implementation UIViewController (RecursiveDescription)
+
+-(void)recursiveDescription
+{
+    NSMutableString *description = [NSMutableString stringWithFormat:@"\n"];
+    [self addDescriptionToString:description indentLevel:0];
+    NSLog(@"%@",description);
+}
+
+-(void)addDescriptionToString:(NSMutableString*)string indentLevel:(NSInteger)indentLevel
+{
+    NSString *padding = [@"" stringByPaddingToLength:indentLevel withString:@" " startingAtIndex:0];
+    [string appendString:padding];
+    [string appendFormat:@"%@, %@",[self debugDescription],NSStringFromCGRect(self.view.frame)];
+
+    for (UIViewController *childController in self.childViewControllers)
+    {
+        [string appendFormat:@"\n%@>",padding];
+        [childController addDescriptionToString:string indentLevel:indentLevel + 1];
+    }
+}
+
+@end
---
+++ readme.md
@@ -0,0 +1,3 @@
+#Recursive description for UIViewController hierarchies
+
+An equivalent of UIView's recursiveDescription method, but for inspecting the view controller hierarchy in an application. 
